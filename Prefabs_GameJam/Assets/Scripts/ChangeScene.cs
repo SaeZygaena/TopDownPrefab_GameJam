6c59e1c5574abfa6e3e6070b77581a76f1002882
SaeZygaena <hugo.altiery@hotmail.fr>
2025-09-15 23:25:33 +0400

    Made Title Game

---
+++ Prefabs_GameJam/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string scene;
+
+    public void ChangingScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
